9bb8ca964cddb9e4f85b8e1857a61bccf9b8aab0
RappyTV <rappytv@rappytv.com>
2025-01-28 18:00:17 +0100

    Improve server state update notifications

--- core/src/main/java/com/rappytv/betterfriends/listeners/FriendServerStateListener.java
+++ core/src/main/java/com/rappytv/betterfriends/listeners/FriendServerStateListener.java
@@ -10,6 +10,7 @@ import net.labymod.api.event.Subscribe;
 import net.labymod.api.event.labymod.labyconnect.session.friend.LabyConnectFriendServerEvent;
 import net.labymod.api.labyconnect.protocol.model.friend.Friend;
 import net.labymod.api.labyconnect.protocol.model.friend.ServerInfo;
+import net.labymod.api.util.time.TimeUtil;
 
 public class FriendServerStateListener {
 
@@ -42,14 +43,34 @@ public class FriendServerStateListener {
             NamedTextColor.DARK_PURPLE
         )));
 
+    String gameModeName = info.getGameModeName();
+    Component text;
+    if (gameModeName == null) {
+      text = Component.translatable("labymod.activity.labyconnect.notifications.server",
+          NamedTextColor.GRAY,
+          address
+      );
+    } else if (TimeUtil.getCurrentTimeMillis() - event.friend().getLastServerChange() < 1000L) {
+      text = Component.translatable(
+          "labymod.activity.labyconnect.notifications.gameModeAndServer",
+          NamedTextColor.GRAY,
+          Component.text(gameModeName, NamedTextColor.AQUA),
+          address
+      );
+    } else {
+      text = Component.translatable(
+          "labymod.activity.labyconnect.notifications.gameMode",
+          NamedTextColor.GRAY,
+          Component.text(gameModeName, NamedTextColor.AQUA)
+      );
+    }
+
     this.addon.displayMessage(
         Component.empty()
             .append(BetterFriendsAddon.prefix)
-            .append(Component.translatable(
-                "betterfriends.notifications.serverUpdate.message",
-                NameHelper.getColoredName(friend),
-                address
-            ))
+            .append(NameHelper.getColoredName(friend))
+            .append(Component.space())
+            .append(text)
             .color(NamedTextColor.GRAY)
     );
   }
