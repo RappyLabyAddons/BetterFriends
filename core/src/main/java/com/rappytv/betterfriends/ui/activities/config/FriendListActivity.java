f07a61a91c6a4b45f9135b2982b2dc558f8a15f1
RappyTV <rappytv@rappytv.com>
2025-03-16 21:29:22 +0100

    Allow custom sorting with dropdown menu

--- core/src/main/java/com/rappytv/betterfriends/ui/activities/config/FriendListActivity.java
+++ core/src/main/java/com/rappytv/betterfriends/ui/activities/config/FriendListActivity.java
@@ -3,7 +3,9 @@ package com.rappytv.betterfriends.ui.activities.config;
 import com.rappytv.betterfriends.ui.widgets.FriendWidget;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.BiFunction;
 import java.util.function.Function;
+import com.rappytv.betterfriends.utils.GroupHelper;
 import net.labymod.api.Laby;
 import net.labymod.api.client.component.Component;
 import net.labymod.api.client.gui.screen.Parent;
@@ -12,6 +14,7 @@ import net.labymod.api.client.gui.screen.activity.Link;
 import net.labymod.api.client.gui.screen.activity.types.SimpleActivity;
 import net.labymod.api.client.gui.screen.widget.widgets.ComponentWidget;
 import net.labymod.api.client.gui.screen.widget.widgets.input.TextFieldWidget;
+import net.labymod.api.client.gui.screen.widget.widgets.input.dropdown.DropdownWidget;
 import net.labymod.api.client.gui.screen.widget.widgets.layout.FlexibleContentWidget;
 import net.labymod.api.client.gui.screen.widget.widgets.layout.ScrollWidget;
 import net.labymod.api.client.gui.screen.widget.widgets.layout.list.HorizontalListWidget;
@@ -37,6 +40,7 @@ public class FriendListActivity<T extends FriendWidget> extends SimpleActivity {
   private final ComponentWidget error = ComponentWidget.empty().addId("error");
   private ScrollWidget scroll;
   private String filterQuery = "";
+  private SortingStrategy sortingStrategy = SortingStrategy.ONLINE_STATUS;
 
   public FriendListActivity(Function<Friend, T> friendWidgetConstructor) {
     this.friendWidgetConstructor = friendWidgetConstructor;
@@ -48,7 +52,8 @@ public class FriendListActivity<T extends FriendWidget> extends SimpleActivity {
 
     FlexibleContentWidget container = new FlexibleContentWidget().addId("container");
 
-    HorizontalListWidget filterSettings = new HorizontalListWidget();
+    HorizontalListWidget filterSettings = new HorizontalListWidget()
+        .addId("filter-settings");
 
     TextFieldWidget searchField = new TextFieldWidget();
     searchField.addId("search-field");
@@ -56,8 +61,18 @@ public class FriendListActivity<T extends FriendWidget> extends SimpleActivity {
     searchField.setText(this.filterQuery);
     searchField.updateListener(this::applyFriendsFilter);
 
-    filterSettings.addEntry(searchField);
+    DropdownWidget<SortingStrategy> sortingDropdown = new DropdownWidget<>();
+    for (SortingStrategy sortingStrategy : SortingStrategy.values()) {
+      sortingDropdown.add(sortingStrategy);
+    }
+    sortingDropdown.setSelected(this.sortingStrategy);
+    sortingDropdown.setTranslationKeyPrefix("betterfriends.settings.advancedFriendlist.sorting");
+    sortingDropdown.setChangeListener(this::applySortingStrategy);
 
+    filterSettings.addEntry(searchField);
+    filterSettings.addEntry(sortingDropdown);
+
+    this.scroll = new ScrollWidget(this.entries);
     this.entries.setComparator((f1, f2) -> {
       if (!(f1 instanceof FriendWidget friendWidget1 && f2 instanceof FriendWidget friendWidget2)) {
         return 0;
@@ -67,30 +82,28 @@ public class FriendListActivity<T extends FriendWidget> extends SimpleActivity {
       int comparePin = Boolean.compare(friend2.isPinned(), friend1.isPinned());
       if (comparePin != 0) {
         return comparePin;
-      } else {
-        int compareStatus = Boolean.compare(friend2.isOnline(), friend1.isOnline());
-        if (compareStatus != 0) {
-          return compareStatus;
-        } else {
-          ChatMessage message1 = friend1.chat().getLastMessage();
-          ChatMessage message2 = friend2.chat().getLastMessage();
-          if (message1 != null || message2 != null) {
-            int compareLastMessage = Long.compare(
-                message2 != null ? message2.getTimestamp() : 0L,
-                message1 != null ? message1.getTimestamp() : 0L
-            );
-            if (compareLastMessage != 0) {
-              return compareLastMessage;
-            }
-          }
-          return Long.compare(friend2.getLastOnline(), friend1.getLastOnline());
+      }
+      int selectedComparator = sortingDropdown.getSelected().compare(friend1, friend2);
+      if (selectedComparator != 0) {
+        return selectedComparator;
+      }
+      ChatMessage message1 = friend1.chat().getLastMessage();
+      ChatMessage message2 = friend2.chat().getLastMessage();
+      if (message1 != null || message2 != null) {
+        int compareLastMessage = Long.compare(
+            message2 != null ? message2.getTimestamp() : 0L,
+            message1 != null ? message1.getTimestamp() : 0L
+        );
+        if (compareLastMessage != 0) {
+          return compareLastMessage;
         }
       }
+      return Long.compare(friend2.getLastOnline(), friend1.getLastOnline());
     });
     this.initializeFriendlist(false);
 
-    this.scroll = new ScrollWidget(this.entries);
     container.addContent(filterSettings);
+    container.addContent(this.error);
     container.addFlexibleContent(this.scroll);
     this.document.addChild(container);
   }
@@ -99,9 +112,11 @@ public class FriendListActivity<T extends FriendWidget> extends SimpleActivity {
     this.entries.getChildren().clear();
     LabyConnectSession session = Laby.references().labyConnect().getSession();
     if (session == null || !session.isAuthenticated()) {
-      this.error.setComponent(Component.text("You are not connected to the LabyChat!"));
-      this.error.setVisible(true);
-      this.scroll.setVisible(false);
+      this.handleError("betterfriends.errors.notConnected");
+      return;
+    }
+    if (session.getFriends().isEmpty()) {
+      this.handleError("betterfriends.errors.emptyList");
       return;
     }
     this.error.setVisible(false);
@@ -128,8 +143,19 @@ public class FriendListActivity<T extends FriendWidget> extends SimpleActivity {
     }
   }
 
+  private void handleError(String translatable) {
+    this.error.setComponent(Component.translatable(translatable));
+    this.error.setVisible(true);
+    this.scroll.setVisible(false);
+  }
+
   private void applyFriendsFilter(String query) {
     this.filterQuery = query;
+    this.initializeFriendlist(true);
+  }
+
+  private void applySortingStrategy(SortingStrategy strategy) {
+    this.sortingStrategy = strategy;
     this.initializeFriendlist(true);
   }
 
@@ -174,5 +200,25 @@ public class FriendListActivity<T extends FriendWidget> extends SimpleActivity {
         FriendWidget.class,
         widget -> widget.getFriend().getUniqueId().equals(event.friend().getUniqueId())
     );
+  }
+
+  private enum SortingStrategy {
+    ONLINE_STATUS((a, b) -> Boolean.compare(b.isOnline(), a.isOnline())),
+    ROLE((a, b) -> Integer.compare(
+        GroupHelper.getGroupIndex(a.gameUser().visibleGroup().getIdentifier()),
+        GroupHelper.getGroupIndex(b.gameUser().visibleGroup().getIdentifier())
+    )),
+    A_TO_Z((a, b) -> a.getName().compareToIgnoreCase(b.getName())),
+    Z_TO_A((a, b) -> b.getName().compareToIgnoreCase(a.getName()));
+
+    private final BiFunction<Friend, Friend, Integer> sortingFunction;
+
+    SortingStrategy(BiFunction<Friend, Friend, Integer> sortingFunction) {
+      this.sortingFunction = sortingFunction;
+    }
+
+    public int compare(Friend f1, Friend f2) {
+      return this.sortingFunction.apply(f1, f2);
+    }
   }
 }
