f9401492f913b8e98151f9fc289f4e0daceaf97c
RappyTV <rappytv@rappytv.com>
2025-11-22 13:41:55 +0100

    chore: Remove redundant code in FriendPinIconTag

--- core/src/main/java/com/rappytv/betterfriends/ui/tags/FriendPinIconTag.java
+++ core/src/main/java/com/rappytv/betterfriends/ui/tags/FriendPinIconTag.java
@@ -5,6 +5,7 @@ import com.rappytv.betterfriends.ui.snapshot.BetterFriendsKeys;
 import net.labymod.api.Textures;
 import net.labymod.api.client.entity.player.tag.tags.IconTag;
 import net.labymod.api.client.gui.icon.Icon;
+import net.labymod.api.labyconnect.protocol.model.friend.Friend;
 
 public class FriendPinIconTag extends IconTag {
 
@@ -24,15 +25,13 @@ public class FriendPinIconTag extends IconTag {
     }
     BetterFriendsFriendSnapshot friendSnapshot = this.snapshot.get(BetterFriendsKeys.FRIEND);
 
-    if (!friendSnapshot.config().enabled().get()
-        || !friendSnapshot.config().pinIconConfig().pinIcon().get()) {
-      return false;
-    }
+    Friend friend = friendSnapshot.friend();
     return super.isVisible()
         && !this.snapshot.isDiscrete()
         && !this.snapshot.isInvisible()
         && friendSnapshot.config().enabled().get()
         && friendSnapshot.config().pinIconConfig().pinIcon().get()
-        && friendSnapshot.friend().isPinned();
+        && friend != null
+        && friend.isPinned();
   }
 }
