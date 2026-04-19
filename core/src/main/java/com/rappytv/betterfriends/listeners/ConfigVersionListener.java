edd9e9639f8b3dcd9bcf43bf800867151ee295ad
RappyTV <rappytv@rappytv.com>
2026-04-19 22:39:45 +0200

    chore: Remove debug log messages

--- core/src/main/java/com/rappytv/betterfriends/listeners/ConfigVersionListener.java
+++ core/src/main/java/com/rappytv/betterfriends/listeners/ConfigVersionListener.java
@@ -9,20 +9,16 @@ public class ConfigVersionListener {
 
   @Subscribe
   public void onConfigVersionUpdate(ConfigurationVersionUpdateEvent event) {
-//    System.out.println(event.getConfigClass() + " " + event.getUsedVersion());
     if (event.getConfigClass() != BetterFriendsConfig.class || event.getUsedVersion() != 1) {
       return;
     }
-    System.out.println(event.getConfigClass() + " " + event.getUsedVersion());
     JsonObject config = event.getJsonObject();
     if (!config.has("prefixColor")) {
       return;
     }
-    System.out.println(config.get("prefixColor").getAsInt());
     JsonObject prefixCustomizationConfig = new JsonObject();
     prefixCustomizationConfig.add("prefixColor", config.get("prefixColor"));
     config.add("prefixCustomizationConfig", prefixCustomizationConfig);
-    System.out.println(config);
 
     event.setJsonObject(config);
   }
